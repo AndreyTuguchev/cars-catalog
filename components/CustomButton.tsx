f4c9257fcb380010a54ef314a4a37de67260e696
Andrey <tuguchev.andrey@gmail.com>
2024-03-25 00:51:31 +0500

    feat: Hero section implemented

---
+++ components/CustomButton.tsx
@@ -0,0 +1,25 @@
+'use client';
+
+import { CustomButtonProps } from '@/types';
+import React from 'react';
+
+let CustomButton = ({
+  title,
+  containerStyles,
+  handleClick,
+}: CustomButtonProps) => {
+  return (
+    <div>
+      <button
+        disabled={false}
+        type={'button'}
+        className={`custom-btn ${containerStyles}`}
+        onClick={handleClick}
+      >
+        <span className={`flex-1`}>{title}</span>
+      </button>
+    </div>
+  );
+};
+
+export default CustomButton;
---
+++ types/index.ts
@@ -0,0 +1,7 @@
+import { MouseEventHandler } from 'react';
+
+export interface CustomButtonProps {
+  title: string;
+  containerStyles?: string; // ?: means optional
+  handleClick?: MouseEventHandler<HTMLButtonElement>; // ?: means optional
+}
